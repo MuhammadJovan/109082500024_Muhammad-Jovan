253db6c76042102dd9fe0c06c7a2d0fe0d1f97f0
MuhammadJovan <mhdjovan45@gmail.com>
2026-04-08 16:44:06 +0700

    fix

--- modul4/soal2.go
+++ modul4/soal2.go
@@ -1,45 +1,46 @@
 package main
 
-import	"fmt"
+import	(
+	"fmt"
+)
 
-func hitungSkor() (int, int) {
-	var soal, skor, waktu int
+func hitungSkor(soal *int, skor *int) {
 
-	soal = 0
-	skor = 0
+	*soal = 0
+	*skor = 0
 
-	for i := 0; i< 8; i++ {
+	var waktu int
+	for i := 0; i < 8; i++ {
 		fmt.Scan(&waktu)
-
+		
 		if waktu <= 300 {
-			soal = soal + 1
-			skor = skor + waktu
+			*soal ++ 
+			*skor += waktu
 		}
 	}
-	return soal, skor
-	
-}
+}	
 
 func main() {
-	var nama string
-	var pemenang string
+	var nama, pemenang string
 	var maxSoal, minSkor int
 	var soal, skor int
 
 	maxSoal = -1
 	minSkor = 999999
 
-	fmt.Scan(&nama)
-
-	for nama != "Selesai"{
-
-		soal, skor = hitungSkor()
-		if soal > maxSoal || (soal == maxSoal && skor < minSkor) {
-			maxSoal = soal
-			minSkor = skor
-			pemenang = nama
-		}
+	for {
 		fmt.Scan(&nama)
+		if nama == "Selesai"{
+		break
+	}
+
+	hitungSkor(&soal, &skor)
+
+	if soal > maxSoal || (soal == maxSoal && skor < minSkor) {
+		maxSoal = soal
+		minSkor = skor
+		pemenang = nama
+		}
 	}
 	fmt.Println(pemenang, maxSoal, minSkor)
 }
